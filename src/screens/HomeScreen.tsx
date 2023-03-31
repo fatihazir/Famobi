4d85bc2564c32dc6875d520e806d0653c6b9ce4f
Fatih HAZIR <fatihhazirceng@gmail.com>
2023-04-01 00:48:40 +0300

    apibase basic games api implementation

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
     View,
     StyleSheet,
@@ -8,9 +8,31 @@ import {
 import { useNavigation } from '@react-navigation/native';
 import { routes } from '../utilities/routes/index';
 import { UseNavigationModel } from '../models/typescript/navigation';
+import apibase from '../utilities/apibase';
+import { links } from '../utilities/apibase/links'
 
 function HomeScreen(): JSX.Element {
     const navigation: UseNavigationModel = useNavigation();
+
+    function GetData() {
+        apibase.Get({
+            url: links.games + '?platform=pcsdsd',
+            successFunction: (res: any) => {
+                console.log("suc", res);
+            },
+            errorFunction: (res: any) => {
+                console.log("error : ", res);
+            },
+            exceptionFunction: (ex: any) => {
+
+            }
+        })
+    }
+
+    useEffect(() => {
+        GetData()
+    }, [])
+
     return (
         <View style={styles.container}>
             <Button
