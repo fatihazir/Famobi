0f7ed4f32d23e9979d45841cc62186cb547e61d4
Fatih HAZIR <fatihhazirceng@gmail.com>
2023-03-31 17:45:46 +0300

    filter screen options and custom header

--- src/navigation/MainStackNavigator.tsx
+++ src/navigation/MainStackNavigator.tsx
@@ -1,19 +1,35 @@
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { routes } from '../utilities/routes';
 import HomeScreen from '../screens/HomeScreen';
 import GameDetailScreen from '../screens/GameDetailScreen';
 import FilterScreen from '../screens/FilterScreen';
+import { Platform, View } from 'react-native';
+import Header from '../components/MainStackNavigator/Header';
 
 const Stack = createStackNavigator();
 
 function MainStackNavigator(): JSX.Element {
+    let isAndroid = Platform.OS == 'android' ? true : false
+
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName={routes.HomeScreen}>
+            <Stack.Navigator
+                initialRouteName={routes.HomeScreen}
+                screenOptions={{
+                    header: (() => <Header />)
+                }}>
                 <Stack.Screen name={routes.HomeScreen} component={HomeScreen} />
                 <Stack.Screen name={routes.GameDetailScreen} component={GameDetailScreen} />
-                <Stack.Screen name={routes.FilterScreen} component={FilterScreen} />
+                <Stack.Screen
+                    name={routes.FilterScreen}
+                    component={FilterScreen}
+                    options={{
+                        headerShown: false,
+                        presentation: 'modal',
+                        gestureEnabled: true,
+                        ...(isAndroid && TransitionPresets.ModalPresentationIOS)
+                    }} />
             </Stack.Navigator>
         </NavigationContainer>
     );
